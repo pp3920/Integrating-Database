574834bbe7961547ec1039acee21e2fa7c6b1282
Priyanka22324 <priyanka22324@gmail.com>
2026-05-21 11:43:30 -0400

    updating productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -15,7 +15,7 @@ If validation fails, it should return a 400 status code with a descriptive error
 
  */
 
-router.post("/", async (req, res) => {
+router.post("/api/products", async (req, res) => {
 
     try {
 
@@ -34,3 +34,60 @@ Retrieves a single product by its _id.
 If the product is found, responds with the product object.
 If no product is found, responds with a 404 status code. */
 
+router.get("api/products/:id", async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.json(product);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+/*
+PUT /api/products/:id (Update a Product)
+
+Updates a product by its _id with the data from req.body.
+Responds with the updated product data (use the { new: true } option).
+If no product is found to update, responds with a 404 status code.
+
+*/
+
+router.put("api/products/:id", async (req, res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+
+        if (!updatedProduct) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.delete("api/products/:id", async (req, res) => {
+    try {
+        const deletedProduct = await Products.findByIdAndDelete(req.params.id);
+
+        if (!deletedProduct) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.json({ message: "Product deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+module.exports = router;
